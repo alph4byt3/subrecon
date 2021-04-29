8ecd7260cdf29deb2b4a292a3c02d6dd1b3c75ed
alph4byt3 <67894940+alph4byt3@users.noreply.github.com>
2021-04-29 23:47:51 +0200

    Update subrecon.sh

--- subrecon.sh
+++ subrecon.sh
@@ -14,33 +14,34 @@ echo "[+] Running Subfinder"
 echo " "
 sleep 3
 
-subfinder -all -config /home/kali/Misc/config.yaml -d $domain -silent -o domains.txt
+subfinder -all -config /home/kali/Misc/config.yaml -d $domain -silent -o subfinder.txt
+
+sleep 3
+echo " "
+echo "[+] Running ShuffleDNS"
+echo " "
+sleep 3
+
+shuffledns -d $domain -w /home/kali/Misc/wordlists/ultimate-subs.txt -r /home/kali/Misc/resolvers.txt -o shuffledns.txt -silent
 
 echo " "
 sleep 3
 echo "[!] Cleaning subdomains"
+cat subfinder.txt > domains.txt
+cat shuffledns.txt >> domains.txt
 cat domains.txt | sort -u > subdomains.txt
 rm domains.txt
-
-echo " "
-sleep 3
-echo "[+] Running dnsx"
-echo " "
-
-dnsx -l subdomains.txt -r /home/kali/Misc/resolvers.txt -o resolved-subdomains.txt -t 1 -silent
-
-echo " "
-sleep 3
-echo "[!] Subdomains resolved"
-rm subdomains.txt
+rm subfinder.txt
+rm shuffledns.txt
 
 echo " "
 sleep 3
 echo "[+] Running httpx"
 echo " "
 
-httpx -l resolved-subdomains.txt -fc 301,302 -threads 1 -o urls.txt -silent
+httpx -l subdomains.txt -fc 301,302 -threads 1 -o urls.txt -silent
 
 echo " "
 sleep 3
+echo "[+] My work here is done..."
 echo " "
