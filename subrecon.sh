e424f9f9b3a8249018a60a71a2fba7f65724c6e0
alph4byt3 <67894940+alph4byt3@users.noreply.github.com>
2021-07-18 00:04:27 +0200

    Update subrecon.sh

--- subrecon.sh
+++ subrecon.sh
@@ -7,33 +7,34 @@ echo -e """\e[33m================================================\e[0m
 \e[33m================================================\e[0m
 """
 
-sleep 3
+sleep 2
 echo -e "Script started - Root domain is \e[31m$domain\e[0m"
 echo " "
+sleep 2
 echo -e "\e[32m[+] Running Subfinder\e[0m"
 echo " "
-sleep 3
+sleep 2
 
 subfinder -all -config /home/kali/Misc/config.yaml -d $domain -o subfinder.txt
 
-sleep 3
 echo " "
+sleep 2
 echo -e "\e[32m[+] Running PureDNS\e[0m"
 echo " "
-sleep 3
+sleep 2
 
 puredns bruteforce /home/kali/Misc/wordlists/ultimate-subs.txt $domain -r /home/kali/Misc/resolvers.txt -w puredns.txt
 
-sleep 3
 echo " "
+sleep 2
 echo -e "\e[32m[+] Running Amass\e[0m"
 echo " "
-sleep 3
+sleep 2
 
 amass enum -d $domain -passive -nolocaldb -config /home/kali/Misc/config.ini -o amass.txt
 
 echo " "
-sleep 3
+sleep 2
 echo -e "\e[33m[!] Cleaning subdomains\e[0m"
 cat subfinder.txt > domains.txt
 cat puredns.txt >> domains.txt
